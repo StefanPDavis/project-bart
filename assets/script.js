6b4aa5728926f409254c60b049096c5bdbfb3cb9
Berie <bberaki2015@gmail.com>
2024-01-12 11:35:00 -0800

    Added js script for modal

--- assets/script.js
+++ assets/script.js
@@ -52,6 +52,78 @@
 //   });
 
 
+// set up local storage
+// set up api's
+// define variables
+// set up button listener
+// set up winners modal
+// create event listener
+// 
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+      $el.classList.add('is-active');
+    }
+  
+    function closeModal($el) {
+      $el.classList.remove('is-active');
+    }
+  
+    function closeAllModals() {
+      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+      });
+    }
+  
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+      const modal = $trigger.dataset.target;
+      const $target = document.getElementById(modal);
+  
+      $trigger.addEventListener('click', () => {
+        openModal($target);
+      });
+    });
+  
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+      const $target = $close.closest('.modal');
+  
+      $close.addEventListener('click', () => {
+        closeModal($target);
+      });
+    });
+  
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+      if (event.code === 'Escape') {
+        closeAllModals();
+      }
+    });
+
+    // Enable draggable functionality on any DOM element. Move the draggable object by clicking on it with the mouse and dragging it anywhere within the viewport.
+
+  $( function() {
+    $( "#draggableCat" ).draggable();
+  } );
+
+    $( function() {
+    $( "#draggableDog" ).draggable();
+  } );
+
+    $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+
+    $( function() {
+    $( "#sortable" ).sortable();
+  } );
+
+  });
+
 document.addEventListener('DOMContentLoaded', () => {
     // Functions to open and close a modal
     function openModal($el) {
